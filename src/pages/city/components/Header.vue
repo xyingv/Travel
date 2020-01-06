198eb64d7bd033274ce8d47929d579397dbf2dbc
unknown <xyingv#163.com>
2020-01-06 16:47:02 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <p>城市选择</p>
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe636;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varible.styl';
+  .header
+    position: relative
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
